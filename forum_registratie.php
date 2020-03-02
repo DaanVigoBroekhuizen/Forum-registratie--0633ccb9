6fff6a48e093eb2483b1d32d5b7545c24ca1e317
daan <2087884@talnet.nl>
2020-03-02 14:22:14 +0100

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>forum registratie</h1>
+
+<form action="registratie_handler.php" method="post" name="post">
+    <input type="text" name="name"> username
+    <input type="text" name="email"> email
+    <input type="number" name="leeftijd"> leeftijd
+    <input type="submit" name="submit">
+</form>
+    
+<?php
+
+
+
+?>
+
+</body>
+</html>
